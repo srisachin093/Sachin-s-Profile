2b1a75529b8b47fa03d8cdf6968a6df89e252c37
Sachin Srivastava <39414091+srisachin093@users.noreply.github.com>
2026-05-24 22:46:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+AOS.init({
+  duration:1200,
+  once:true
+});
+
+new Typed("#typing", {
+  strings:[
+    "DevOps Engineer",
+    "AWS Specialist",
+    "Kubernetes Engineer",
+    "Cloud Automation Expert"
+  ],
+  typeSpeed:60,
+  backSpeed:40,
+  loop:true
+});
